5010e9352a69b3686f6bf53e4dde129aeb367069
Justin Branco <justin.brancode@gmail.com>
2018-06-18 14:09:14 -0400

    Initial commit

---
+++ mvdl.py
@@ -0,0 +1,28 @@
+import pytube
+import urllib.request
+import urllib.parse
+import re
+
+print("Enter the video name: ")
+song = input()
+
+query_string = urllib.parse.urlencode({"search_query" : song})
+html_content = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+
+yt = pytube.YouTube("https://www.youtube.com/watch?v=" + search_results[0])
+videos = yt.get_videos()
+
+s = 1
+for v in videos:
+	print(str(s) + ". " + str(v))
+	s += 1
+
+print("Enter the number of the video: ")
+n = int(input())
+vid = videos[n-1]
+
+destination = 'F:\Python Projects\Youtube Downloader\Music Videos'
+vid.download(destination)
+
+print(yt.filename + "\nHas been successfully downloaded.")
